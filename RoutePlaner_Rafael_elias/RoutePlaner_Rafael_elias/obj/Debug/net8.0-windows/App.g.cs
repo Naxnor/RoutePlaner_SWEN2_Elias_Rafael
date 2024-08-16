04c954a7c6f038fcc526da55223a68105e0940db
Elias <127867888+EliasKnr@users.noreply.github.com>
2024-08-16 13:32:47 +0200

    Setup
    First commit elias_ast

--- RoutePlaner_Rafael_elias/RoutePlaner_Rafael_elias/obj/Debug/net8.0-windows/App.g.cs
+++ RoutePlaner_Rafael_elias/RoutePlaner_Rafael_elias/obj/Debug/net8.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace RoutePlaner_Rafael_elias {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace RoutePlaner_Rafael_elias {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             RoutePlaner_Rafael_elias.App app = new RoutePlaner_Rafael_elias.App();
             app.InitializeComponent();
