b6679f0585b0ca0280099d2b657626b6b6eb863d
Elias <127867888+EliasKnr@users.noreply.github.com>
2024-08-30 23:10:18 +0200

    FINAL111

--- RoutePlaner_Rafael_elias/RoutePlaner_Rafael_elias/Migrations/20240830210732_InitialCreate.Designer.cs
+++ RoutePlaner_Rafael_elias/RoutePlaner_Rafael_elias/Migrations/20240830210732_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using RoutePlaner_Rafael_elias.Database;
 namespace RoutePlaner_Rafael_elias.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240830193604_InitialCreate")]
+    [Migration("20240830210732_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
